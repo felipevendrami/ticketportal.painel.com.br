3b83ebc8ef28066a40c5e1db08a15c37ef94e90a
Davi Lemes <ndlemes7@outlook.com>
2023-06-25 11:46:19 -0300

    rotas adicionadas

--- src/Api/Service.js
+++ src/Api/Service.js
@@ -4,8 +4,50 @@ const clientApi = axios.create({
     baseURL: 'http://localhost:8080/'
 });
 
-export const registrarEventoAPI = (evento) => clientApi.post(`/evento`,evento);
+export const registrarUsuarioAPI = (usuario) => clientApi.post(`/usuarios`, usuario);
 
-export const retornaTodosEventosAPI = () => clientApi.get(`/eventos`);
+export const deletarUsuarioAPI = (id) => clientApi.delete(`/usuarios/${id}`);
 
-export const registrarTipoIngressoAPI = (evento) => clientApi.post(`/tipo_ingresso`,evento);
+export const getUsuarioAPI = (id) => clientApi.get(`/usuarios/${id}`);
+
+export const getTodosUsuariosAPI = () => clientApi.get(`/usuarios`);
+
+export const registrarEventoAPI = (evento) => clientApi.post(`/eventos`, evento);
+
+export const deletarEventoAPI = (id) => clientApi.delete(`/eventos/${id}`);
+
+export const getEventoAPI = (id) => clientApi.get(`/eventos/${id}`);
+
+export const getTodosEventosAPI = () => clientApi.get(`/eventos`);
+
+export const registrarCompraAPI = (compra) => clientApi.post(`/compras`, compra);
+
+export const deletarCompraAPI = (id) => clientApi.delete(`/compras/${id}`);
+
+export const getCompraAPI = (id) => clientApi.get(`/compras/${id}`);
+
+export const getTodasComprasAPI = () => clientApi.get(`/compras`);
+
+export const registrarItemCompraAPI = (itemCompra) => clientApi.post(`/itens-compra`, itemCompra);
+
+export const deletarItemCompraAPI = (id) => clientApi.delete(`/itens-compra/${id}`);
+
+export const getItemCompraAPI = (id) => clientApi.get(`/itens-compra/${id}`);
+
+export const getTodosItensCompraAPI = () => clientApi.get(`/itens-compra`);
+
+export const registrarTipoIngressoAPI = (tipoIngresso) => clientApi.post(`/tipos-ingresso`, tipoIngresso);
+
+export const deletarTipoIngressoAPI = (id) => clientApi.delete(`/tipos-ingresso/${id}`);
+
+export const getTipoIngressoAPI = (id) => clientApi.get(`/tipos-ingresso/${id}`);
+
+export const getTodosTiposIngressoAPI = () => clientApi.get(`/tipos-ingresso`);
+
+export const registrarIngressoAPI = (ingresso) => clientApi.post(`/ingressos`, ingresso);
+
+export const deletarIngressoAPI = (id) => clientApi.delete(`/ingressos/${id}`);
+
+export const getIngressoAPI = (id) => clientApi.get(`/ingressos/${id}`);
+
+export const getTodosIngressosAPI = () => clientApi.get(`/ingressos`);
